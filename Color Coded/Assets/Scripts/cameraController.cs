36ff8c09944dc0d017b7e4405b3fadc1ae2cc49a
Akash Pathuri <akash.pathuri@gmail.com>
2020-07-08 22:31:39 -0400

    Add Unity Project

---
+++ Color Coded/Assets/Scripts/cameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour{
+
+    private const float min = -50.0f;
+    private const float max = 0.0f;
+    public Transform lookAtPlayer;
+    public float cameraDistance; 
+    public float x;
+    public float z;
+    public float currentX = 190f;
+    public float currentY = -15.0f;
+    public Transform body;
+
+
+    private void Update(){
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+        
+        currentY = Mathf.Clamp(currentY, min, max);
+    }
+
+    private void LateUpdate() {
+        Vector3 dir = new Vector3(x, z, -cameraDistance);
+        Quaternion rotation = Quaternion.Euler(-currentY, currentX, 0);
+
+        transform.position = lookAtPlayer.position + rotation * dir;
+        transform.LookAt(lookAtPlayer.position);
+        transform.position = new Vector3(transform.position.x + x, transform.position.y + 1f, transform.position.z + z);
+
+        Quaternion torque = Quaternion.Euler(0, currentX, 0);
+        body.rotation = torque;
+
+    }
+}
---
+++ Color Coded/Assets/Scripts/playerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour{
+
+    public float jumpforce, movementSpeed;
+    float moveVertical, moveHorizontal;
+    bool jump;
+    Rigidbody body;
+
+
+    void Start(){
+        Cursor.lockState = CursorLockMode.Locked;
+        body = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update(){
+        moveVertical = Input.GetAxis("Vertical");
+        moveHorizontal = Input.GetAxis("Horizontal");
+        jump = Input.GetButton("Jump");
+        transform.position += transform.forward * Time.deltaTime * (movementSpeed * moveVertical);
+        transform.position += transform.right * Time.deltaTime * (movementSpeed * moveHorizontal);
+
+
+        if (jump)
+        {
+            body.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        }
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+    private void FixedUpdate(){
+
+        
+    }
+}
