7e044f23ad58c0c76e6a78e896595a8ee808029a
Akash Pathuri <akash.pathuri@gmail.com>
2020-07-15 22:19:30 -0400

    implemented animations

--- Color Coded/Assets/Scripts/playerController.cs
+++ Color Coded/Assets/Scripts/playerController.cs
@@ -5,36 +5,70 @@ using UnityEngine;
 public class playerController : MonoBehaviour{
 
     public float jumpforce, movementSpeed;
-    float moveVertical, moveHorizontal;
+    float forwardMovement, sideMovement;
     bool jump;
     Rigidbody body;
+    Animator animationController;
+    bool ready2Jump = false;
 
 
     void Start(){
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
+        animationController = gameObject.GetComponent<Animator>();
         body = gameObject.GetComponent<Rigidbody>();
+        //controller = gameObject.AddComponent<CharacterController>();
     }
 
     void Update(){
-        moveVertical = Input.GetAxis("Vertical");
-        moveHorizontal = Input.GetAxis("Horizontal");
+        forwardMovement = Input.GetAxis("Vertical");
+        sideMovement = Input.GetAxis("Horizontal");
         jump = Input.GetButton("Jump");
-        transform.position += transform.forward * Time.deltaTime * (movementSpeed * moveVertical);
-        transform.position += transform.right * Time.deltaTime * (movementSpeed * moveHorizontal);
-
-
-        if (jump)
+        if(forwardMovement != 0)
         {
-            body.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            animationController.SetBool("run",true);
+            animationController.SetFloat("runSpeed", forwardMovement);
+        }else if (forwardMovement == 0)
+        {
+            animationController.SetBool("run", false);
+            animationController.SetFloat("runSpeed", 0);
         }
+        transform.position += transform.forward * Time.deltaTime * (movementSpeed * forwardMovement);
+        transform.position += transform.right * Time.deltaTime * (movementSpeed * sideMovement);
+
+
+        if (jump && ready2Jump)
+        {
+            //animationController.SetTrigger("runJump");
+            body.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        }if(!ready2Jump && body.velocity.y < 0)
+        {
+            body.velocity += Vector3.up * Physics.gravity.y * 2f * Time.deltaTime;
+        }
+        Debug.Log(ready2Jump);
+
+
+
+
         if (Input.GetKeyDown("escape"))
         {
             Cursor.lockState = CursorLockMode.None;
         }
 
     }
-    private void FixedUpdate(){
 
-        
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag.Equals("Ground")){
+            //Debug.Log("Grounded");
+            ready2Jump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.collider.tag.Equals("Ground")){
+           // Debug.Log("Not Grounded");
+           ready2Jump = false;
+        }
     }
 }
