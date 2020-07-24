e80b6fe9e12960de4df0318178e0d7570469f020
Akash Pathuri <akash.pathuri@gmail.com>
2020-07-23 21:34:03 -0400

    implemented player mechanics

--- Color Coded/Assets/Scripts/playerController.cs
+++ Color Coded/Assets/Scripts/playerController.cs
@@ -6,60 +6,108 @@ public class playerController : MonoBehaviour{
 
     public float jumpforce, movementSpeed;
     float forwardMovement, sideMovement;
-    bool jump;
+    bool jump, jumped;
     Rigidbody body;
     Animator animationController;
     bool ready2Jump = false;
+    public Material health;
+    float r = 0;
+    float g = 255;
+    public float initial;
 
 
     void Start(){
         //Cursor.lockState = CursorLockMode.Locked;
         animationController = gameObject.GetComponent<Animator>();
         body = gameObject.GetComponent<Rigidbody>();
-        //controller = gameObject.AddComponent<CharacterController>();
     }
 
     void Update(){
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            initial = Time.time;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            float change = Time.time - initial;
+            Debug.Log(change);
+            if (change > 1f)
+            {
+                animationController.SetTrigger("attack");
+                //transform.Translate(Vector3.forward * 5f);
+            }
+            else
+            {
+                animationController.SetTrigger("slash");
+
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            animationController.SetTrigger("block");
+            
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            animationController.SetTrigger("letgo");
+        }
+
         forwardMovement = Input.GetAxis("Vertical");
         sideMovement = Input.GetAxis("Horizontal");
         jump = Input.GetButton("Jump");
-        if(forwardMovement != 0)
+        if(forwardMovement > 0)
         {
             animationController.SetBool("run",true);
             animationController.SetFloat("runSpeed", forwardMovement);
-        }else if (forwardMovement == 0)
+        }else if (forwardMovement < 0)
+        {
+            animationController.SetBool("backUp", true);
+            animationController.SetFloat("runSpeed", forwardMovement);
+        }
+        else if (forwardMovement == 0)
         {
             animationController.SetBool("run", false);
+            animationController.SetBool("backUp", false);
             animationController.SetFloat("runSpeed", 0);
+        }
+
+        if (sideMovement > 0)
+        {
+            animationController.SetBool("right", true);
+        }
+        else if (sideMovement < 0)
+        {
+            animationController.SetBool("left", true);
+        }
+        else if (sideMovement == 0)
+        {
+            animationController.SetBool("left", false);
+            animationController.SetBool("right", false);
         }
         transform.position += transform.forward * Time.deltaTime * (movementSpeed * forwardMovement);
         transform.position += transform.right * Time.deltaTime * (movementSpeed * sideMovement);
-
-
-        if (jump && ready2Jump)
-        {
-            //animationController.SetTrigger("runJump");
-            body.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
-        }if(!ready2Jump && body.velocity.y < 0)
-        {
-            body.velocity += Vector3.up * Physics.gravity.y * 2f * Time.deltaTime;
-        }
-        Debug.Log(ready2Jump);
-
-
 
 
         if (Input.GetKeyDown("escape"))
         {
             Cursor.lockState = CursorLockMode.None;
         }
-
+    }
+    private void FixedUpdate()
+    {
+        if (jump && ready2Jump)
+        {
+            animationController.SetTrigger("jump");
+            body.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.tag.Equals("Ground")){
-            //Debug.Log("Grounded");
+            Debug.Log("Grounded");
             ready2Jump = true;
         }
     }
@@ -67,8 +115,10 @@ public class playerController : MonoBehaviour{
     private void OnCollisionExit(Collision collision)
     {
         if(collision.collider.tag.Equals("Ground")){
-           // Debug.Log("Not Grounded");
-           ready2Jump = false;
+           Debug.Log("Not Grounded");
+            animationController.ResetTrigger("jump");
+
+            ready2Jump = false;
         }
     }
 }
